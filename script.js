9c11e396c6a49b7bf2eb5ffa264cb9ea038d9d3c
Zaps Senior dev <zapssrdev@outlook.com>
2024-04-19 11:01:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var elem = document.querySelectorAll(".elem");
+
+elem.forEach(function(val){
+    val.addEventListener("mouseenter",function(){
+        val.childNodes[3].style.opacity = 1
+
+    });
+    val.addEventListener("mouseleave",function(){
+        val.childNodes[3].style.opacity = 0
+    });
+    val.addEventListener("mousemove",function(dets){
+        val.childNodes[3].style.left = dets.x+"px"
+        // val.childNodes[3].style.top = dets.y+"px"
+        // val.childNodes[3].style.top = 10+"px"
+    });
+});
+
+// var elemImage = document.querySelector(".elem img")
+
+// elem.addEventListener("mousemove", function(dets){
+//     elemImage.style.left = dets.x+"px"
+//     elemImage.style.top = dets.y+"px"
+// })
+// elem.addEventListener("mouseenter",function(){
+//     elemImage.style.opacity = 1
+// })
+// elem.addEventListener("mouseleave",function(){
+//     elemImage.style.opacity = 0
+// })
